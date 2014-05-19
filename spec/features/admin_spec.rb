4d25973cc03d1f806cc42dd2c093f87ef85f8ece
Scott Skender <sjskender@gmail.com>
2014-05-19 16:49:08 -0600

    [#71496782] Admins see a link to add a publisher
    Signed-off-by: Mark Evans <twenty4play@gmail.com>

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 feature 'Admin users' do
-  scenario 'Admin visits a users page and logs in' do
+  before do
     User.create!(
       email: 'admin@example.com',
       password: 'password',
       password_confirmation: 'password',
       admin: true
     )
+    User.create!(
+      email: 'not-admin@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      admin: false
+    )
+  end
+  scenario 'Admin visits a users page and logs in' do
 
     visit '/login'
 
@@ -28,5 +36,39 @@ feature 'Admin users' do
 
     expect(page).to have_content 'invalid username/password'
 
+  end
+
+  scenario 'Admins see a link to add a publisher' do
+    visit '/'
+    expect(page).to_not have_link 'Add Publisher'
+
+    visit '/login'
+
+    fill_in 'session[email]', with: 'admin@example.com'
+    fill_in 'session[password]', with: 'password'
+    click_on 'Login'
+
+    expect(page).to have_content 'Welcome admin@example.com'
+
+    within('nav') do
+      expect(page).to have_link 'Add Publisher'
+    end
+  end
+
+  scenario 'Non admins cannot see a link to add a publisher' do
+    visit '/'
+    expect(page).to_not have_link 'Add Publisher'
+
+    visit '/login'
+
+    fill_in 'session[email]', with: 'not-admin@example.com'
+    fill_in 'session[password]', with: 'password'
+    click_on 'Login'
+
+    expect(page).to have_content 'Welcome not-admin@example.com'
+
+    within('nav') do
+      expect(page).to_not have_link 'Add Publisher'
+    end
   end
 end
