4f9bde868b8013b3ee8c20126c58427852b99f23
Scott Skender <sjskender@gmail.com>
2014-05-20 10:20:02 -0600

    [#71496782] Refactor
    Moved admin? functionality from view to controller
    
    Signed-off-by: Martha Berner <marthaberner.mb@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def is_admin?
+    if session[:id].present? && User.find(session[:id]).admin
+      true
+    end
+  end
+
+  helper_method :is_admin?
 end
