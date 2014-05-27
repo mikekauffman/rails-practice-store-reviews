c7f136636de6328c20edd992fa4f9dacb64c3c22
Scott Skender <sjskender@gmail.com>
2014-05-27 16:49:48 -0600

    [#71614768] Users must register with a unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
             presence: true,
             format: {with: /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i, message: " is invalid"}
 
+  validates :email,
+            uniqueness: true
+
   validates :password,
             format: {with: /\A^(?=.*\d)(?=.*[a-zA-Z]).{8,12}$\z/, message: "must be at least 8-12 characters with 1 number"}
 
