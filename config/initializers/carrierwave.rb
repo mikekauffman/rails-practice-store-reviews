69e5e0d941a6d65be8c72a6fa4d5fa828a477c8c
Cory Leistikow <cory.leistikow@gmail.com>
2014-06-04 16:22:30 -0600

    added fake hack to carrierwave config to fix deploy fail
    Signed-off-by: Paul Stone <pmstone247@gmail.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS', # required
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'], # required
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], # required
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'] || "fake", # required
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] || "fake", # required
 
     }
     config.fog_directory = ENV['MY_BUCKET'] # required
